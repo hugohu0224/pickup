cd7b6074f4c13613c37ad63b5c05d1897528dd82
hugohu0224 <hugohu0224@gmail.com>
2024-07-25 23:02:16 +0800

    Refactor client.go to make it cleaner and easier to understand.

--- internal/game/client.go
+++ internal/game/client.go
@@ -3,7 +3,6 @@ package game
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"go.uber.org/zap"
@@ -12,98 +11,110 @@ import (
 )
 
 type Client struct {
-	ID              string
-	Hub             *Hub
-	Conn            *websocket.Conn
-	Action          chan *models.Action
-	Send            chan *models.GameMsg
-	Done            chan struct{}
-	DefaultPosition models.Position
-	mu              sync.Mutex
+	ID   string
+	Hub  *Hub
+	Conn *websocket.Conn
+	Send chan *models.GameMsg
+	Done chan struct{}
+	mu   sync.Mutex
 }
 
 func NewClient(id string, hub *Hub, conn *websocket.Conn) *Client {
 	return &Client{
-		ID:     id,
-		Hub:    hub,
-		Conn:   conn,
-		Action: nil,
-		Send:   make(chan *models.GameMsg, 128),
-		Done:   make(chan struct{}),
-		mu:     sync.Mutex{},
+		ID:   id,
+		Hub:  hub,
+		Conn: conn,
+		Send: make(chan *models.GameMsg, 128),
+		Done: make(chan struct{}),
 	}
 }
 
-func gameMsgContentSwaper[T any](gameMsg *models.GameMsg) (*T, error) {
+func gameMsgContentSwapper[T any](gameMsg *models.GameMsg) (*T, error) {
 	var structInstance T
 	contentBytes, err := json.Marshal(gameMsg.Content)
 	if err != nil {
-		zap.S().Errorf("gameMsg.Content json marshal failed: %v", err)
-		return nil, err
+		return nil, fmt.Errorf("gameMsg.Content json marshal failed: %w", err)
 	}
-	if err = json.Unmarshal(contentBytes, &structInstance); err != nil {
-		zap.S().Errorf("json unmarshal failed: %v", err)
-		return nil, err
+	if err := json.Unmarshal(contentBytes, &structInstance); err != nil {
+		return nil, fmt.Errorf("json unmarshal failed: %w", err)
 	}
 	return &structInstance, nil
-
 }
 
 func (c *Client) ReadPump(ctx context.Context) error {
-	zap.S().Infof("ReadPump start Client: %v\n", c.ID)
+	zap.S().Infof("ReadPump start Client: %v", c.ID)
+	defer c.Hub.ClientManager.RemoveClient(c)
 
 	for {
-		var gameMsg models.GameMsg
-		err := c.Conn.ReadJSON(&gameMsg)
-		if err != nil {
-			c.Hub.ClientManager.RemoveClient(c)
-			return err
-		}
-		switch gameMsg.Type {
-		case models.PlayerPositionType:
-			position, err := gameMsgContentSwaper[models.PlayerPosition](&gameMsg)
-			if err != nil {
-				return err
-			}
-			zap.S().Debugf("ReadPump msg: %v", gameMsg)
-
-			position.ID = c.ID
-			c.Hub.PositionChan <- position
-		case models.ItemActionType:
-			itemAction, err := gameMsgContentSwaper[models.ItemAction](&gameMsg)
-			if err != nil {
-				return err
-			}
-			itemAction.ID = c.ID
-			c.Hub.ActionChan <- itemAction
-
-		case models.PlayerChatMsgType:
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("ReadPump for client %s stopped due to context cancellation", c.ID)
 		default:
-			c.Hub.ClientManager.RemoveClient(c)
-			zap.S().Errorf("invalid gameMsg type: %v", gameMsg.Type)
+			var gameMsg models.GameMsg
+			if err := c.Conn.ReadJSON(&gameMsg); err != nil {
+				return fmt.Errorf("failed to read JSON: %w", err)
+			}
+
+			if err := c.handleGameMsg(&gameMsg); err != nil {
+				return fmt.Errorf("failed to handle game message: %w", err)
+			}
 		}
 	}
 }
 
+func (c *Client) handleGameMsg(gameMsg *models.GameMsg) error {
+	switch gameMsg.Type {
+	case models.PlayerPositionType:
+		return c.handlePlayerPosition(gameMsg)
+	case models.ItemActionType:
+		return c.handleItemAction(gameMsg)
+	case models.PlayerChatMsgType:
+
+		return nil
+	default:
+		return fmt.Errorf("invalid gameMsg type: %v", gameMsg.Type)
+	}
+}
+
+func (c *Client) handlePlayerPosition(gameMsg *models.GameMsg) error {
+	position, err := gameMsgContentSwapper[models.PlayerPosition](gameMsg)
+	if err != nil {
+		return err
+	}
+	position.ID = c.ID
+	c.Hub.PositionChan <- position
+	return nil
+}
+
+func (c *Client) handleItemAction(gameMsg *models.GameMsg) error {
+	itemAction, err := gameMsgContentSwapper[models.ItemAction](gameMsg)
+	if err != nil {
+		return err
+	}
+	itemAction.ID = c.ID
+	c.Hub.ActionChan <- itemAction
+	return nil
+}
+
 func (c *Client) WritePump(ctx context.Context) error {
-	zap.S().Infof("WritePump start Client: %v\n", c.ID)
-	defer func() {
-		c.Conn.Close()
-	}()
+	zap.S().Infof("WritePump start Client: %v", c.ID)
+	defer c.Conn.Close()
+
 	for {
 		select {
 		case <-ctx.Done():
-			errors.New(fmt.Sprintf("WritePump for client %s stopped due to context cancellation", c.ID))
+			return fmt.Errorf("WritePump for client %s stopped due to context cancellation", c.ID)
 		case msg, ok := <-c.Send:
-			zap.S().Debugf("WritePump msg: %v", msg)
 			if !ok {
-				errors.New(fmt.Sprintf("failed to send message to client: %v", c.ID))
-				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return c.writeCloseMessage()
 			}
-			err := c.Conn.WriteJSON(msg)
-			if err != nil {
-				return err
+			if err := c.Conn.WriteJSON(msg); err != nil {
+				return fmt.Errorf("failed to write JSON: %w", err)
 			}
 		}
 	}
+}
+
+func (c *Client) writeCloseMessage() error {
+	return c.Conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 }
